170ac55e8f7ae0a1e978e8c1ceb26161323f1ed3
nolleh <nolleh7707@gmail.com>
2016-09-27 22:22:12 +0900

    ing

--- chap.7/quadtree.hs
+++ chap.7/quadtree.hs
@@ -1,16 +1,27 @@
 import Data.List
--- weave = groupBy (\x y -> if snd x == 'x' then ) $
---   zip [0..]
--- quadTree = 
+import Debug.Trace
 
+weave :: String -> [String]
+weave = 
+  foldl 
+    (\acc x -> 
+      -- ing.. x 가 기존에 있었던 경우에도 합쳐버린다. 음 이건 안되는데 ~ 
+        if x == 'x' then trace (show "acctake:" ++ show acc) $ accTake acc ++ [concat (xData acc)]
+        else acc ++ [[x]]) []
+  where sizeAcc acc = length acc
+        xData acc = drop (sizeAcc acc - 4) acc
+        accTake acc = take (sizeAcc acc - 4) acc
 
-runQuadTree =
-  print $ quadTree "w"
-  print $ quadTree "xbwwb" -- xwbbw
+quadTree xs = trace (reverse xs) 
+  weave $ reverse xs
+
+main = do
+  -- print $ ["b", "b", "w"] ++ [concat ["b", "b", "w"]] 
+  -- print $ map (\x -> [x]) [1,2,3,4]
+  -- print $ 'x':'b':'c':[]
+  -- print $ quadTree "w"
+  -- print $ quadTree "xbwwb" -- xwbbw
 -- xbwxwbbwb -> x b w x(wbbw) b -> x x(wbbw) b b w -> x x(bwwb) b b w
 -- [b, w, [w, b, b, w] b]
   print $ quadTree "xbwxwbbwb" -- xxbwwbbbw
-  print $ quadTree "xxwwwbxwxwbbbwwxxxwwbbbwwwwbb" -- xxwbxwwxbbwwbwbxwbwwxwwwxbbwb
-
-main = do
-  print $ runQuadTree
+  -- print $ quadTree "xxwwwbxwxwbbbwwxxxwwbbbwwwwbb" -- xxwbxwwxbbwwbwbxwbwwxwwwxbbwb
